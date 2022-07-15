cdd3b9ac8d00cbadc00eda258f2853118911a3e7
mortdekai <alexstomson1984@gmail.com>
2022-07-15 20:13:03 +0300

    add day 1

---
+++ src/main/java/day1/Task6.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int k = 6;
+
+
+        for(int i = 0; i<=9 ; i++){
+           int y = k * i;
+            System.out.println( i + " x " + k + " = " + y);
+        }
+    }
+}
